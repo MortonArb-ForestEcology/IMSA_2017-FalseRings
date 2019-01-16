d367fcde3f08563ab324541d9fb17cbc0f0f65cf
Bethany Zumwalde <bzumwalde@mortonarb.org>
2019-01-16 12:49:16 -0600

    reading in data
    Reading in tree and Jessica's data

--- scripts_workflow/01_organize_datasets.R
+++ scripts_workflow/01_organize_datasets.R
@@ -10,4 +10,19 @@
 # 5. Calculate cumulative area for each landmark (EW/LW boundary point)
 # 6. Calculate area of each latewood/earlywood portion (EWA, LWA)
 
-# Libraries needed: dplR, stingr, ggplot2 (for graphing)
+
+
+library(dplyr); library(ggplot2); library(stringr); library(googlesheets)
+
+setwd("C:/Users/BZumwalde/Desktop/IMSA_2017-FalseRings/data_raw")
+
+#Reading in Jessica's data
+xylem <- gs_title("Xylem_Data")
+xylem.df <- data.frame(gs_read(xylem, ws="raw data"))
+summary(xylem.df)
+#Still need to set as some columns as numbers i.e. vessel
+
+#Reading in Tree Data
+plotsurvey <- read.csv("./TreeSurveyData/Tree_PlotSurvey_2017 - raw data.csv")
+#survey.add <- read.csv("./TreeSurveyData/Trees_Survey_Additional_URF_2017 - raw_data.csv")
+

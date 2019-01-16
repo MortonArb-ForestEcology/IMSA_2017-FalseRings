59d9b1ea62589b37aed4cd3373bc97c5461a1924
Christy Rollinson <crollinson@gmail.com>
2019-01-16 11:06:31 -0600

    repo cleanup!
    Adding some raw data; re-arranging some things

---
+++ scripts_workflow/01_organize_datasets.R
@@ -0,0 +1,11 @@
+# Purpose: This script will gather all of the tree data we need and put it into a single data frame.
+# Authors: Christy Rollinson, Bethany Zumwalde, Jessica Oros
+# 
+# Workflow:
+# 1. Read in tree data & Jessica's Data
+# 2. Subset to just the trees Jessica needs
+# 3. Read in the tree ring measurements for Jessica's trees
+# ------ Maybe split into new script? ------ 
+# 4. Calculate diameter for each EW/LW boundary point
+# 5. Calculate cumulative area for each landmark (EW/LW boundary point)
+# 6. Calculate area of each latewood/earlywood portion (EWA, LWA)

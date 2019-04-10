e88a9b676b259cff1cf2b2c5029c908c7fc28630
Christy Rollinson <crollinson@gmail.com>
2019-04-10 16:13:11 -0500

    Put LMEM p-values into tables

--- scripts_workflow/02_analysis_species.R
+++ scripts_workflow/02_analysis_species.R
@@ -18,51 +18,6 @@ ggplot(data=ring.stats) +
   geom_histogram(aes(x=Area.Cond.Tot, fill=Species)) 
 
 
-png(file.path(path.figs, "TotalConductiveArea.png"), height=5.5, width=7.5, units="in", res=180)
-ggplot(data=ring.stats) +
-  geom_boxplot(aes(x=Species, y=Area.Cond.Tot, fill=Species)) +
-  scale_fill_manual(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)"), values=c("cadetblue3", "coral2")) +
-  scale_x_discrete(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)")) +
-  scale_y_continuous(name=expression(bold(paste("Total Conductive Area (mm"^"2", " yr"^"-1", ")"))), limits=c(0, 4.5), expand=c(0,0)) +
-  guides(fill=F) +
-  theme_bw() +
-  theme(panel.grid=element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y=element_text(size=rel(2), face="bold", color="black"),
-        axis.text.x=element_text(size=rel(2), face="bold.italic", color="black"),
-        axis.text.y=element_text(size=rel(2), color="black"))
-dev.off()
-
-png(file.path(path.figs, "VesselDensity.png"), height=5.5, width=7.5, units="in", res=180)
-ggplot(data=ring.stats) +
-  geom_boxplot(aes(x=Species, y=Vessel.Density, fill=Species)) +
-  scale_fill_manual(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)"), values=c("cadetblue3", "coral2")) +
-  scale_x_discrete(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)")) +
-  scale_y_continuous(name=expression(bold(paste("Vessel Density (vessels mm"^"-2", " yr"^"-1",")")))) +
-  guides(fill=F) +
-  theme_bw() +
-  theme(panel.grid=element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y=element_text(size=rel(2), face="bold", color="black"),
-        axis.text.x=element_text(size=rel(2), face="bold.italic", color="black"),
-        axis.text.y=element_text(size=rel(2), color="black"))
-dev.off()
-
-png(file.path(path.figs, "VesselArea.png"), height=5.5, width=7.5, units="in", res=180)
-ggplot(data=ring.stats) +
-  geom_boxplot(aes(x=Species, y=Vessel.Area, fill=Species)) +
-  scale_fill_manual(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)"), values=c("cadetblue3", "coral2")) +
-  scale_x_discrete(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)")) +
-  scale_y_continuous(name=expression(bold(paste("Mean Vessel Area (mm"^"2",")")))) +
-  guides(fill=F) +
-  theme_bw() +
-  theme(panel.grid=element_blank(),
-        axis.title.x = element_blank(),
-        axis.title.y=element_text(size=rel(2), face="bold", color="black"),
-        axis.text.x=element_text(size=rel(2), face="bold.italic", color="black"),
-        axis.text.y=element_text(size=rel(2), color="black"))
-dev.off()
-
 # Comparing Total conductive area across species
 # Testing for differences in year; no difference, but we need to keep the year effect so our data is accounted for properly
 mod.tca.rml <- lme(Area.Cond.Tot ~ Species, data=ring.stats, random=list(IMLS_Plot=~1, Core.ID=~1, year=~1), method="REML")
@@ -75,7 +30,7 @@ anova(mod.tca.rml, mod.tca.rml2, mod.tca.rml3)
 mod.tca <- lme(Area.Cond.Tot ~ Species, data=ring.stats, random=list(year=~1, IMLS_Plot=~1, Core.ID=~1), method="ML")
 # mod.tca2 <- lme(Area.Cond.Tot ~ Species, data=ring.stats, random=list(year=~1, IMLS_Plot=~1, Core.ID=~1))
 sum.tca <- summary(mod.tca)
-sum.tca$coefficients$fixed[2]/sum.tca$coefficients$fixed[1] # Looking at the percent difference
+sum.tca$tTable[2,1]/sum.tca$sum.va$tTable[1,1] # Looking at the percent difference
 
 # summary(mod.tca2)
 # summary(mod.tca.rml)
@@ -94,7 +49,7 @@ summary(mod.vd2)
 
 # Looking at the percent difference
 sum.vd <- summary(mod.vd)
-sum.vd$coefficients$fixed[2]/sum.vd$coefficients$fixed[1] # Looking at the percent difference
+sum.vd$tTable[2,1]/sum.vd$sum.va$tTable[1,1] # Looking at the percent difference
 
 hist(resid(mod.vd))
 plot(predict(mod.vd) ~ ring.stats$Vessel.Density)
@@ -113,7 +68,7 @@ summary(mod.va)
 
 # Looking at the percent difference
 sum.va <- summary(mod.va)
-sum.va$coefficients$fixed[2]/sum.va$coefficients$fixed[1] # Looking at the percent difference
+sum.va$tTable[2,1]/sum.va$sum.va$tTable[1,1] # Looking at the percent difference
 
 hist(resid(mod.va))
 plot(predict(mod.va) ~ ring.stats$Vessel.Area)
@@ -122,3 +77,51 @@ plot(predict(mod.va) ~ ring.stats$Vessel.Area)
 hist(resid(mod.va2))
 plot(predict(mod.va2) ~ ring.stats$Vessel.Area)
 # plot(resid(mod.va2) ~ predict(mod.va2))
+
+png(file.path(path.figs, "TotalConductiveArea.png"), height=6, width=9, units="in", res=180)
+ggplot(data=ring.stats) +
+  geom_boxplot(aes(x=Species, y=Area.Cond.Tot, fill=Species)) +
+  annotate(geom="text", x=2.35, y=0.35, label=paste0("p=", round(sum.tca$tTable[2,5], 2)), size=8) +
+  scale_fill_manual(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)"), values=c("cadetblue3", "coral2")) +
+  scale_x_discrete(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)")) +
+  scale_y_continuous(name=expression(bold(paste("Total Conductive Area (cm"^"2", " yr"^"-1", ")"))), limits=c(0, 4.5), expand=c(0,0)) +
+  guides(fill=F) +
+  theme_bw() +
+  theme(panel.grid=element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y=element_text(size=rel(2), face="bold", color="black"),
+        axis.text.x=element_text(size=rel(2), face="bold.italic", color="black"),
+        axis.text.y=element_text(size=rel(2), color="black"))
+dev.off()
+
+png(file.path(path.figs, "VesselDensity.png"), height=6, width=9, units="in", res=180)
+ggplot(data=ring.stats) +
+  geom_boxplot(aes(x=Species, y=Vessel.Density, fill=Species)) +
+  annotate(geom="text", x=2.35, y=1, label=paste0("p=", round(sum.vd$tTable[2,5], 2)), size=8) +
+  scale_fill_manual(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)"), values=c("cadetblue3", "coral2")) +
+  scale_x_discrete(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)")) +
+  scale_y_continuous(name=expression(bold(paste("Vessel Density (vessels mm"^"-2", " yr"^"-1",")")))) +
+  guides(fill=F) +
+  theme_bw() +
+  theme(panel.grid=element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y=element_text(size=rel(2), face="bold", color="black"),
+        axis.text.x=element_text(size=rel(2), face="bold.italic", color="black"),
+        axis.text.y=element_text(size=rel(2), color="black"))
+dev.off()
+
+png(file.path(path.figs, "VesselArea.png"), height=6, width=9, units="in", res=180)
+ggplot(data=ring.stats) +
+  geom_boxplot(aes(x=Species, y=Vessel.Area, fill=Species)) +
+  annotate(geom="text", x=2.35, y=0.035, label=paste0("p=", round(sum.va$tTable[2,5], 2)), size=8) +
+  scale_fill_manual(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)"), values=c("cadetblue3", "coral2")) +
+  scale_x_discrete(labels=c("Q. alba\n(White Oak)", "Q. rubra\n(Red Oak)")) +
+  scale_y_continuous(name=expression(bold(paste("Mean Vessel Area (mm"^"2",")")))) +
+  guides(fill=F) +
+  theme_bw() +
+  theme(panel.grid=element_blank(),
+        axis.title.x = element_blank(),
+        axis.title.y=element_text(size=rel(2), face="bold", color="black"),
+        axis.text.x=element_text(size=rel(2), face="bold.italic", color="black"),
+        axis.text.y=element_text(size=rel(2), color="black"))
+dev.off()
